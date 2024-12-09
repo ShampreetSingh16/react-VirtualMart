ba8a791e18ee4fc1b401d4751bded1992487e45f
Shampreet Singh <113058369+ShampreetSingh16@users.noreply.github.com>
2024-12-09 00:24:58 -0500

    Update ProductDescription.tsx

--- app/src/components/products/ProductDescription.tsx
+++ app/src/components/products/ProductDescription.tsx
@@ -23,7 +23,7 @@ const ProductDescription = () => {
     useEffect(() => {
         const fetchProduct = async () => {
             try {
-                const response = await axios.get(`http://localhost:8000/products/${id}`);
+                const response = await axios.get(`https://virtualmartserver.onrender.com/products/${id}`);
                 setProduct(response.data);
             } catch (error) {
                 setError(true);
